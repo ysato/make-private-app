e21ca8c0e1a3f3ba701cc34e230ba443d92bda88
ysato <56924527+ysato@users.noreply.github.com>
2022-11-27 20:41:50 +0900

    fix: Added /ping routing

--- api/github/webhooks/index.ts
+++ api/github/webhooks/index.ts
@@ -1,8 +1,20 @@
-const {createNodeMiddleware, createProbot} = require('probot');
+import {createNodeMiddleware, createProbot} from 'probot';
+import {VercelRequest, VercelResponse} from '@vercel/node'
+import app from '../../../src';
 
-const app = require('../../../src/index');
-
-export = createNodeMiddleware(app, {
+const middleware = createNodeMiddleware(app, {
     probot: createProbot(),
     webhooksPath: '/api/github/webhooks'
 });
+
+export = (request: VercelRequest, response: VercelResponse) => {
+    if (request.method !== 'POST') {
+        response.writeHead(302, {
+            Location: '/ping'
+        });
+        response.end();
+        return;
+    }
+
+    middleware(request, response);
+};
